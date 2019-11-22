ee2566ee2940f9cb7ac6cfa65b5afc5e279e15b7
vladimir <vladimir.smetankin@similarbid.com>
2019-11-22 18:45:18 +0200

    fix api protocol

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,10 +1,10 @@
 import { makeAPICall } from './helper';
 
-const API_KEY = 'UD4SCAX3zLfke0bHjEDBBzaHNKSWBYs0';
+const API_KEY = 'rXX6gRAGas7dEvJyz21j3Z6pWMRKaJnk';
 
 export const getCityKeyByName = async (name) =>{
 
-    const location = await makeAPICall(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${name}`);
+    const location = await makeAPICall(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${name}`);
 
     const firstCity = location[0];
     if (!firstCity) {
@@ -14,12 +14,12 @@ export const getCityKeyByName = async (name) =>{
 };
 
 export const getWeatherByCityKey = async (cityKey) =>{
-    const cityInfo = await makeAPICall(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}`);
+    const cityInfo = await makeAPICall(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}`);
     return cityInfo.DailyForecasts;
 };
 
 export const getWeatherInfoByCity = async (cityName) =>{
     const cityKey = await getCityKeyByName(cityName);
-    const cityInfo = await makeAPICall(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}`);
+    const cityInfo = await makeAPICall(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}`);
     return cityInfo.DailyForecasts;
 };
